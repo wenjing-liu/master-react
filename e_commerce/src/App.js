c4a45f862fd505ec1d4482a68e4c6f161f8cc821
claire liu <claire_liu@epam.com>
2022-07-20 18:56:03 +0800

    e_commerce cart

--- e_commerce/src/App.js
+++ e_commerce/src/App.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { commerce } from './lib/commerce';
 
-import { Products, Navbar } from './components';
+import { Products, Navbar, Cart } from './components';
 const App = () => {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  }
+
+  const fetchCart = async () => {
+    const cart = await commerce.cart.retrieve();
+    setCart(cart);
+  }
+
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+
+    setCart(item.cart);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+    fetchCart();
+  }, []);
+
   return (
     <div>
-      <Navbar />
-      <Products />
+      <Navbar totalItems={cart.total_items} />
+      {/* <Products products={products} onAddToCart={handleAddToCart}/> */}
+      <Cart cart={cart}/>
     </div>
   )
 }
