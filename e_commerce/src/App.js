f6b423d641bf64724da4ea0f7c070e123474c139
claire liu <claire_liu@epam.com>
2022-07-21 11:32:03 +0800

    e_commerce add update cart

--- e_commerce/src/App.js
+++ e_commerce/src/App.js
@@ -15,13 +15,32 @@ const App = () => {
 
   const fetchCart = async () => {
     const cart = await commerce.cart.retrieve();
+
     setCart(cart);
   }
 
   const handleAddToCart = async (productId, quantity) => {
-    const item = await commerce.cart.add(productId, quantity);
+    const { cart } = await commerce.cart.add(productId, quantity);
 
-    setCart(item.cart);
+    setCart(cart);
+  }
+
+  const handleUpdateCartQty = async (productId, quantity) => {
+    const { cart } = await commerce.cart.update(productId, { quantity });
+
+    setCart(cart);
+  }
+
+  const handleRemoveFromCart = async (productId) => {
+    const { cart } = await commerce.cart.remove(productId);
+
+    setCart(cart);
+  }
+
+  const handleEmptyCart = async () => {
+    const { cart } = await commerce.cart.empty();
+
+    setCart(cart);
   }
 
   useEffect(() => {
@@ -35,7 +54,14 @@ const App = () => {
         <Navbar totalItems={cart.total_items} />
         <Routes>
           <Route path="/" element={<Products products={products} onAddToCart={handleAddToCart}/>}></Route>
-          <Route path="/cart" element={<Cart cart={cart}/>}>
+          <Route path="/cart" element={
+            <Cart
+              cart={cart}
+              handleUpdateCartQty={handleUpdateCartQty}
+              handleRemoveFromCart={handleRemoveFromCart}
+              handleEmptyCart={handleEmptyCart}
+            />
+          }>
           </Route>
         </Routes>
       </div>
